5d48351640ed71a53e2b4c90f18111eb69bbf1ca
bodhibrdr <julie.gumerman@gmail.com>
2019-09-04 16:18:04 -0600

    MVP

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
+    //setting state, snagging any items from storage
     const [storedValue, setStoredValue] = useState(() => {
         const item = window.localStorage.getItem(key);
         return item ? JSON.parse(item) : initialValue;
     });
+
+    //setting state if there isn't already something in storage; this will be saved to local storage. (setter function)
     const setValue = value => {
-        setStoredValue(value);
-        window.localStorage.setItem(key, JSON.stringify(value));
-    };
+        setStoredValue(value); //setting state of stored value with value
+        window.localStorage.setItem(key, JSON.stringify(value)); //and now it's being stashed in local storage
+    }
 
     return [storedValue, setValue];
+
 }
 
 export default useLocalStorage;
